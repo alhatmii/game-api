eff9fd42bbb6ff00d74efe2521bbdd1a7b5f7170
alhatmii <alhatmii.150@gmail.com>
2023-07-28 17:25:46 +0400

    Added new function to create game using CopyOnWriteArrayList

--- src/main/java/com/techytribe/gameapi/PlayerController.java
+++ src/main/java/com/techytribe/gameapi/PlayerController.java
@@ -1,10 +1,22 @@
 package com.techytribe.gameapi;
 
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.CopyOnWriteArrayList;
 
 @RestController
 @RequestMapping(path = "/api/v1/player")
 public class PlayerController {
+    // We use this type of list to handel multiple request, create duplicate of list, put data in different list then
+    //marge it
+    CopyOnWriteArrayList <Player> listOfPlayers = new CopyOnWriteArrayList<>();
+//To create game we use post mapping
+    @PostMapping
+    public Player createPlayer(Player incomingPlayer){
+        listOfPlayers.add(incomingPlayer);
+        return incomingPlayer;
 
+    }
 }
